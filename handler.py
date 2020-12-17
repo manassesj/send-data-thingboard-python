2584ac00f8e2e406d83572d1d04913041e881bfe
manasses <jmanasses1024@hotmail.com>
2020-12-17 14:58:25 -0300

    package change and connection to thingsboard

--- handler.py
+++ handler.py
@@ -1,26 +1,59 @@
-import paho.mqtt.client as paho  # mqtt library
+from tb_device_mqtt import TBDeviceMqttClient, TBPublishInfo
+import base64
 import json
 
 def hello(event, context):
 
-    ACCESS_TOKEN = 'ERdDIEYFsMjunS8YSxe3'  # Token of your device
-    broker = "demo.thingsboard.io"  # host name
-    port = 1883  # data listening port
 
-    def on_publish(client, userdata, result):  # create function for
-        print("data published to thingsboard \n")
-        pass
+    telemetry = []
 
-    client1 = paho.Client("control1")  # create client object
-    client1.on_publish = on_publish  # assign function to
-    client1.username_pw_set(ACCESS_TOKEN)  # access token from
-    client1.connect(broker, port, keepalive=60)  # establish connection
+    for result in event['Records']:
+        base64_message = result['kinesis']['data']
+        base64_bytes = base64_message.encode('ascii')
+        message_bytes = base64.b64decode(base64_bytes)
+        message = message_bytes.decode('ascii')
 
-    payload = json.dumps({
-        "name": "PYTHON3",
-        "index": 0
-    })
+        message = json.loads(message)
 
-    ret = client1.publish("v1/devices/me/telemetry", payload)
-    print("Please check LATEST TELEMETRY field of your device")
-    print(payload);
+        if message['heat_index'] != 0:
+            celsius  = (message['heat_index'] - 32) / 1.8
+
+            if celsius <= 27.0:
+                message['warning'] = "Normal"
+            elif celsius >= 27.0 and celsius <= 32.0:
+                message['warning'] = "Caution"
+            elif  celsius >= 32.1 and celsius <= 41.0:
+                message['warning'] = "Extreme caution"
+            elif celsius >= 41.1 <= 54.0:
+                message['warning'] = "Danger"
+            else:
+                message['warning'] = "Extreme danger"
+
+            message['heat_index'] = celsius
+            
+
+        else:
+            message['warning'] = "Normal"
+        telemetry.append(message)
+
+    print(telemetry)
+
+    try:
+        #telemetry = [{"uf": "fd","index": 20.5}, {"uf": "df","index": 110.5}]
+        client = TBDeviceMqttClient("demo.thingsboard.io", "ONISUjegn6ndL5GBIHNf")
+        client.connect()
+
+        for station in telemetry:
+            print(station)
+            result = client.send_telemetry(station)
+            #print(result)
+            success = result.get() == TBPublishInfo.TB_ERR_SUCCESS
+            #print(success)
+        client.disconnect()
+
+
+
+    except:
+        print("Unable to send telemetry")
+
+    # serverless invoke local --function hello
